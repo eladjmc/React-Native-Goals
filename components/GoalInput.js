5cf579fc59bb1834a16aff69b82f68aa9873dcc8
eladjmc <eladjmc88@gmail.com>
2023-05-29 12:43:47 +0300

    changed some styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,11 @@
-import { View, Button, TextInput, StyleSheet, Modal } from "react-native";
+import {
+  View,
+  Button,
+  TextInput,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
 
 const GoalInput = ({
   enteredGoalText,
@@ -10,6 +17,7 @@ const GoalInput = ({
   return (
     <Modal visible={isModalOpen} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require("../assets/goal.png")} />
         <TextInput
           onChangeText={goalInputHandler}
           value={enteredGoalText}
@@ -18,10 +26,14 @@ const GoalInput = ({
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Add Goal" color="#b180f0" onPress={addGoalHandler} />
           </View>
           <View style={styles.button}>
-            <Button title="Close" onPress={toggleModalHandler} />
+            <Button
+              title="Close"
+              color="#f31282"
+              onPress={toggleModalHandler}
+            />
           </View>
         </View>
       </View>
@@ -34,29 +46,34 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    padding:16,
+    padding: 16,
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#311b6b",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    borderRadius:6,
     width: "100%",
     marginLeft: 8,
     padding: 8,
   },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
   buttonContainer: {
     flexDirection: "row",
-    justifyContent:"center",
-    width: '60%',
-    alignItems:"center",
+    justifyContent: "center",
+    width: "60%",
+    alignItems: "center",
     marginTop: 16,
   },
   button: {
-    width: '50%',
-    marginHorizontal:8,
-  }
+    width: "50%",
+    marginHorizontal: 8,
+  },
 });
